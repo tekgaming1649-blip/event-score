3ff112225bd1d718803c0d1d40af281deedb0cb0
tekgaming1649-blip <tekgaming1649@gmail.com>
2026-08-13 13:18:37 +0000

    Fix Firebase config for score-event project

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,7 @@
 window.FIREBASE_CONFIG = {
   apiKey: 'AIzaSyC4W0uT7fXrxV6H7pGkQ7-r7VvU4',
   authDomain: 'score-de-l-event-default-rtdb.firebaseapp.com',
-  databaseURL: 'https://score-de-l-event-default-rtdb.firebaseio.com',
+  databaseURL: 'https://score-de-l-event-default-rtdb.europe-west1.firebasedatabase.app',
   projectId: 'score-de-l-event-default-rtdb',
   storageBucket: 'score-de-l-event-default-rtdb.appspot.com',
   messagingSenderId: '542712067318',
